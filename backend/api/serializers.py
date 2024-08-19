614bac9074487cf99d22a686f4799bea04fb9323
prabhasydv <142390148+prabhasydv@users.noreply.github.com>
2024-08-19 18:51:17 +0530

    first commit

---
+++ backend/api/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Note  # Import the Note model
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validate_data):
+        print(validate_data)
+        user = User.objects.create_user(**validate_data)
+        return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ["id", "title", "content", "created_at", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
---
+++ frontend/src/components/Loadingindicator.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../styles/Loadingindicator.css"
+
+const Loadingindicator = () => {
+  return (
+    <div className='loading-container'>
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default Loadingindicator
